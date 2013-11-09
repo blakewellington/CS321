368af153777a713ae0b8f6f157f9b1c556e917d7
Blake Wellington <blakewellington@gmail.com>
2013-11-08 16:10:41 -0800

    Added blank Assignment3 folder to the project - No work done yet

---
+++ Assignment3/tst/err23.java
@@ -0,0 +1,5 @@
+class testd23 {
+  public static void main(String[] a) {
+    main = 1;  // invalid lhs
+  }
+}
---
+++ Assignment3/tst/test14.java
@@ -0,0 +1,19 @@
+// test method invocations
+// (should print 2)
+class test {
+  public static void main(String[] a) {
+    A a = new A();
+    System.out.println(a.go());
+  }
+}
+
+class A {
+  public int back(int a) {
+    return a;
+  }
+
+  public int go() {
+    //    return this.back(2);
+    return back(2);
+  }
+}
